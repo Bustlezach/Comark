80acf497c928087a99f27b59f0f5d6edf75b37c2
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2023-08-30 22:56:56 +0100

    update product

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import (
-    Flask, render_template, jsonify, request, flash, redirect, url_for
+    Flask, render_template, request, flash, redirect, url_for
 )
-from user import user
-# from geopy.geocoders import Nominatim
+
+from geopy.geocoders import Nominatim
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
     LoginManager, UserMixin, login_user, login_required, logout_user
@@ -16,28 +16,36 @@ app.config["SECRET_KEY"] = "random string"
 db = SQLAlchemy()
 login_manager = LoginManager(app)
 login_manager.init_app(app)
-app.register_blueprint(user, url_prefix="/user")
 bcrypt = Bcrypt(app)
 
 
-
 class User(db.Model, UserMixin):
+    """
+    This class provides data for the users
+    table in the database
+    """
     __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     phone_number = db.Column(db.String, nullable=False)
-    address = db.Column(db.Text, nullable=False)
+    address = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
     state = db.Column(db.String, nullable=False)
     country = db.Column(db.String)
+
+    products = db.relationship('Product', backref='users ')
 
     def __repr__(self):
         return f"Username: {self.username}, Name: {self.name}, Phone Number: {self.phone_number}"
 
 
 class Product(db.Model, UserMixin):
+    """
+    This class provides data for the products
+    table in the database
+    """
     __tablename__ = "products"
     product_id = db.Column(db.Integer, primary_key = True)
     product_name = db.Column(db.String, nullable=False)
@@ -51,6 +59,7 @@ class Product(db.Model, UserMixin):
         return f"The product name is {self.product_name}, and it costs {self.price}."
 
 
+"""Initialise the database"""
 db.init_app(app)
 with app.app_context():
     db.create_all()
@@ -62,36 +71,56 @@ def loader_user(user_id):
     return User.query.get(user_id)
 
 
-# def get_coords():
-#     client = Nominatim(user_agent="my_app")
-#     location = client.geocode("My current location")
-#     lat = location.latitude
-#     long = location.longitude
-#     return lat, long  # Return both latitude and longitude
+def get_coords():
+    client = Nominatim(user_agent="my_app")
+    location = client.geocode("My current location")
+    lat = location.latitude
+    long = location.longitude
+#    return lat, long  # Return both latitude and longitude
+    KEY = '7CjHpcOpgKkcKI73yNKxUSyGZdzJKmZn'
+    url = f'https://www.mapquestapi.com/geocoding/v1/reverse?key={KEY}&location={lat},{long}&includeRoadMetadata=true&includeNearestIntersection=true'
+    message = requests.get(url)
+    if (message.status_code is 200):
+        res = message.json()
+        results = res.get('results')
+        city = results[0]['locations'][0]['adminArea5']
+        return city
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """The landing page route"""
+    products = Product.query.all()
+    return render_template('index.html', products=products)
 
 
 @app.route('/about')
 def about():
+    """The about page route"""
     return render_template('about.html')
 
 
 @app.route('/team')
 def team():
+    """The team page route"""
     return render_template('team.html')
 
 
 @app.route('/sign_up')
 def sign_up():
+    """The sign up page route."""
     return render_template('sign_up.html')
 
 
 @app.route('/register_user', methods=['POST'])
 def register_user():
+    """
+    The form filled in the sign up page is submitted
+    to register route where it is processed and
+    stored in the database on success and redirected to
+    sign up page on failure.
+    """
     if request.method == 'POST':
         name = request.form['name']
         username = request.form['username']
@@ -102,10 +131,10 @@ def register_user():
         country = request.form['country']
 
         password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
-        
+
         user = User(
-            username=username, password=password, name=name, 
-            phone_number=phone_number, address=address, location=location, 
+            username=username, password=password, name=name,
+            phone_number=phone_number, address=address, location=location,
             state=state, country=country
             )
         
@@ -114,16 +143,6 @@ def register_user():
         flash(f"{username}, you have been registered!")
         return redirect(url_for('index'))
     return render_template('sign_up.html')
-
-
-
-
-# @app.route('/login', methods=['POST'])
-# def login():
-#     if request.method == 'POST':
-#         if not request.form['username'] \
-#             or not requests.form['password']:
-#             flash("Please, enter the fields.", "error")
 
 
 # @app.route('/receive_coordinates', methods=['POST'])
@@ -141,6 +160,93 @@ def register_user():
 #             city = results[0]['locations'][0]['adminArea5']
 #             return city
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        if not request.form['username'] \
+        or not request.form['password']:
+            flash("Fill in your username and password to login.")
+        else:
+            user = User.query.filter_by(username=
+                request.form['username']
+            ).first()
+            if user and bcrypt.check_password_hash(
+                user.password, request.form['password']
+                ):
+                login_user(user)
+                flash(f"Welcome, {user.name}!")
+                return redirect(url_for('user_page', user_id=user.id))
+            else:
+                flash('Invalid username or password')
+    return redirect(url_for('index'))
+
+
+@login_required
+@app.route('/user/<user_id>')
+def user_page(user_id):
+    posts = Product.query.filter_by(user_id=user_id).all()
+    return render_template('user.html', title='User Homepage', posts=posts)
+
+
+@login_required
+@app.route('/user/addPost', methods=['POST'])
+def add_post():
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        category = request.form['category']
+        price = request.form['price']
+        description = request.form['description']
+        img_link = request.form['image_link']
+        product = Product(
+            product_name=product_name, category=category, price=price,
+            description=description, img_link=img_link
+            )
+        
+        db.session.add(product)
+        db.session.commit()
+        flash(f"{product_name} is posted successfully.")
+        return redirect(url_for('user_page'))
+    return render_template('add_post.html', title='Create Product')
+
+
+@login_required
+@app.route('/user/update<int:product_id>', methods=['POST', 'GET'])
+def update(product_id):
+    product = Product.query.filter_by(product_id=product_id).first()
+    if request.method == 'POST':
+        new_product_name = request.form['product_name']
+        new_category = request.form['category']
+        new_price = request.form['price']
+        new_description = request.form['description']
+        new_img_link = request.form['image_link']
+        product.product_name = new_product_name
+        product.category = new_category
+        product.price = new_price
+        product.description = new_description
+        product.img_link = new_img_link
+
+        db.session.commit()
+        flash(f"{new_product_name} has been updated successfully.")
+        return redirect(url_for('user_page'))
+
+
+
+@login_required
+@app.route('/user/delete/<product_id>')
+def delete_product(product_id):
+    product = Product.query.filter_by(product_id=product_id).first()
+    db.session.delete(product)
+    db.session.commit()
+    flash("Product deleted.")
+    return redirect(url_for('user_page'))
+
+
+@login_required
+@app.route('/user/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
