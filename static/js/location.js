dc0408bf77ba984be9eb02a77e2235a2e90c8068
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2023-09-02 14:37:33 +0100

    index products

---
+++ static/js/location.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const getLocationButton = document.querySelector(".form-btn");
+
+    getLocationButton.addEventListener("click", () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                sendToFlask(latitude, longitude);
+            }, function (error) {
+                console.error("Error getting geolocation:", error.message);
+            });
+        } else {
+            console.error("Geolocation is not available in this browser.");
+        }
+    });
+
+
+    // Function to send latitude and longitude to the Flask app
+    function sendToFlask(latitude, longitude) {
+        fetch("C:\\Users\\hp\\Desktop\\Comark\\static\\js\\location.js", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            position: JSON.stringify({ latitude, longitude }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data sent to Flask app:", data);
+        })
+        .catch(error => {
+            console.error("Error sending data to Flask app:", error);
+        });
+    }
+});
